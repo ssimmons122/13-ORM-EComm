b015d4393da2c27eb0a7861bb6e5c45ae675a6f9
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-04-25 23:24:28 -0400

    sequelize, server on

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+//
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`Listening on port ${PORT}. Let's go!`);
 });
